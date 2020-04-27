12539b11c753d08b44b1e58e24dd16a2bf4f609a
root <david.ducos@percona.com>
2020-04-27 18:47:28 +0000

    changes added

--- myloader_pt-osc.sh
+++ myloader_pt-osc.sh
@@ -3,16 +3,20 @@
 # -T tablename
 # -t amoount of threads for myloader
 
+function myecho {
+  echo "$(date ): $*"
+}
+
 working_path=$PWD
 pk_column=id
 min_id=1
 chunk_size=1000
 threads=4
 credentials=""
+
 while [[ $# -gt 0 ]]
 do
 key="$1"
-incremental_path=""
 case $key in
     -D|--database)
     database="$2"
@@ -106,14 +110,13 @@ fi
 ptosc_log=".ptosc.log"
 touch /tmp/pause.file
 
-pt-online-schema-change --dry-run --alter="$alter_statement" --print D=${database},t=${table_name} > ${ptosc_log}
+pt-online-schema-change $credentials --dry-run --alter="$alter_statement" --print D=${database},t=${table_name} > ${ptosc_log}
 insert_statement=$(grep INSERT ${ptosc_log})
-#table_name_new=$( grep "ALTER TABLE" ${ptosc_log} | cut -d'`' -f4)
 
 if [ "$insert_statement" == "" ]
 then
   echo "Empty insert statement"
-  exit
+  exit 1
 fi
 
 bin_path="${working_path}/.bin"
@@ -123,47 +126,43 @@ mkdir -p ${bin_path}
 
 wget percona.com/get/pt-online-schema-change > /dev/null 2>&1
 chmod u+x pt-online-schema-change
+rm ${bin_path}/pt-online-schema-change
 mv pt-online-schema-change $bin_path
 
 ptosc=${bin_path}/pt-online-schema-change
 
 version=$( ${ptosc} --version | cut -d' ' -f2 )
 ptosc_patch_filename=pt-online-schema-change.${version}.patch
+rm $ptosc_patch_filename
 
 wget ${git_url}/${ptosc_patch_filename} > /dev/null 2>&1
 
 patch ${ptosc} ${ptosc_patch_filename}
 
-rm $ptosc_patch_filename
-
-
 
 ptosc_log="${bin_path}/ptosc.log"
 ptosc_pause="${bin_path}/pause_file"
 touch $ptosc_pause
-
+myecho "Starting pt-osc "
 $ptosc --pause-file=$ptosc_pause --alter="$alter_statement" --no-data --execute D=${database},t=${table_name} > ${ptosc_log} & 
-sleep 2
+process_id=$!
+sleep 0.2
 schema_file="${database}.${table_name}-schema.sql"
 
 # It can be empty:
 > ${working_path}/metadata
 > ${working_path}/${schema_file}
 
-
 if [ "${max_id}" == "" ];
 then
   max_id=$( echo "select max(${pk_column}) from ${table_name}" | mysql $credentials $database -NA )
 fi
 
-
-
-# Creating the INSERT INTO files
-i=0
+echo "Creating the INSERT INTO files"
 last=$(( ($max_id - $min_id ) / $chunk_size + 1 ))
 
 from=${min_id}
-for i in $(seq -w 00000 $last)
+for i in $(seq -w 00000 $last | sort -R)
 do
   to=$(( $from + $chunk_size - 1 ))
   filename="${working_path}/${database}.${table_name}.${i}.sql"
@@ -171,13 +170,11 @@ do
   from=$(( $to + 1 ))
 done
 
-echo "Starting myloader"
+myecho "Starting myloader"
 # We are reading to import the data
-myloader -d ${working_path} -t $threads $credentials 
+/root/git/my/mydumper/myloader -q 1 -d ${working_path} -t $threads $credentials 
 
-
-echo "Renaming Tables"
-table_name_old=_${table_name}_old
-#echo "RENAME TABLE ${database}.${table_name} TO ${database}.${table_name_old} , ${database}.${table_name_new} TO ${database}.${table_name}; DROP TABLE ${database}.${table_name_old} " | mysql $credentials $database
-
+myecho "Removing pause file"
 rm $ptosc_pause
+myecho "Waiting to pt-osc to rename tables..."
+wait $process_id
